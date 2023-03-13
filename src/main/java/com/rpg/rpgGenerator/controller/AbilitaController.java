a46bb29841dbbd698de63bf2d0dd5b1228e51f13
AntoCle <clemenzaantonino96@gmail.com>
2023-03-13 18:18:36 +0100

    abilita controller

--- src/main/java/com/rpg/rpgGenerator/controller/AbilitaController.java
+++ src/main/java/com/rpg/rpgGenerator/controller/AbilitaController.java
@@ -1,4 +1,67 @@
 package com.rpg.rpgGenerator.controller;
 
+import com.rpg.rpgGenerator.entity.Abilita;
+import com.rpg.rpgGenerator.service.AbilitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/abilita")
 public class AbilitaController {
+
+    @Autowired
+    private AbilitaService abilitaService;
+
+    @GetMapping("")
+    public List<Abilita> findAll() {
+        return abilitaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Abilita> findById(@PathVariable String id) {
+        Optional<Abilita> abilita = abilitaService.findById(id);
+        return abilita.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/cerca-per-nome-abilita/{nomeAbilita}")
+    public ResponseEntity<Abilita> findByNomeAbilita(@PathVariable String nomeAbilita) {
+        Abilita abilita = abilitaService.findByNomeAbilita(nomeAbilita);
+        return abilita != null ? ResponseEntity.ok(abilita) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/cerca-per-nome-attributi-abilita/{nomeAttributiAbilita}")
+    public List<Abilita> findAllByNomeAttributiAbilitaContaining(@PathVariable String nomeAttributiAbilita) {
+        return abilitaService.findAllByNomeAttributiAbilitaContaining(nomeAttributiAbilita);
+    }
+
+    @PostMapping("")
+    public Abilita create(@RequestBody Abilita abilita) {
+        return abilitaService.insertAbilita(abilita);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Abilita> update(@PathVariable String id, @RequestBody Abilita abilita) {
+        Optional<Abilita> existingAbilita = abilitaService.findById(id);
+        if (existingAbilita.isPresent()) {
+            Abilita updatedAbilita = abilitaService.save(abilita);
+            return ResponseEntity.ok(updatedAbilita);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        Optional<Abilita> abilita = abilitaService.findById(id);
+        if (abilita.isPresent()) {
+            abilitaService.delete(abilita.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
