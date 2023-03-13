96827363e9f6db37e4a41ddd91763036fdfb8eee
AntoCle <clemenzaantonino96@gmail.com>
2023-03-13 16:38:22 +0100

    Repository fix

--- src/main/java/com/rpg/rpgGenerator/repository/CategoriaRepository.java
+++ src/main/java/com/rpg/rpgGenerator/repository/CategoriaRepository.java
@@ -2,7 +2,18 @@ package com.rpg.rpgGenerator.repository;
 
 import com.rpg.rpgGenerator.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+    @Query("SELECT c FROM Categoria c WHERE c.nomeAttributiCategoria LIKE %?1%")
+    List<Categoria> findByNomeAttributiCategoriaContaining(String keyword);
+
+    @Query("SELECT COUNT(c) FROM Categoria c")
+    Long countAll();
+
+    @Query("SELECT c.nomeAttributiCategoria FROM Categoria c")
+    List<String> findAllNames();
 }
